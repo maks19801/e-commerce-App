5fd35f6bb0804b0012cd949c99e10abd471f2d92
Порховский Максим <maks19801@gmail.com>
2021-10-19 22:29:56 +0300

    added new brand and type entities, json files with data seeding

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -2,6 +2,7 @@
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
+using System.Reflection;
 
 namespace Infrastructure.Data
 {
@@ -9,8 +10,16 @@ namespace Infrastructure.Data
   {
     public StoreContext(DbContextOptions<StoreContext> options) : base(options)
     {
+
     }
 
     public DbSet<Product> Products{get; set;}
+    public DbSet<ProductType> ProductTypes { get; set; }
+    public DbSet<ProductBrand> ProductBrands {get; set; }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      base.OnModelCreating(modelBuilder);
+      modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
   }
 }
