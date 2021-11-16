cce425311f679fc97f2af89343e6522c6e06c955
Порховский Максим <maks19801@gmail.com>
2021-11-16 17:47:49 +0200

    added routes, links to the components, class active to links

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -4,6 +4,7 @@ import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -12,9 +13,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ShopComponent,
     ProductItemComponent,
     ProductDetailsComponent,
-    
+
   ],
-  imports: [CommonModule, SharedModule],
+  imports: [CommonModule, SharedModule, RouterModule],
   exports: [ShopComponent],
 })
 export class ShopModule {}
