4a7adc533351877f569e2eb9c8da81c767d80aba
Порховский Максим <maks19801@gmail.com>
2021-11-16 16:55:14 +0200

    added home and product-details components

--- client/src/app/shop/shop.module.ts
+++ client/src/app/shop/shop.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ShopComponent } from './shop.component';
 import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 
@@ -10,6 +11,7 @@ import { SharedModule } from '../shared/shared.module';
   declarations: [
     ShopComponent,
     ProductItemComponent,
+    ProductDetailsComponent,
     
   ],
   imports: [CommonModule, SharedModule],
